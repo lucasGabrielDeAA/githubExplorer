c28eac3bd0fa202de58ce39ac67ccbb21cacc58e
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-06 21:54:43 -0300

    Styling StatusBar

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import {
+  StatusBar,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import styles from './styles';
 
 const Welcome = () => (
-    <View style={styles.container}>
-        <Text style={styles.title}>Bem vindo</Text>
-        <Text style={styles.text}>
-            Para continuar, é necessário que você informe seu usuário no GitHub
-        </Text>
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.title}>Bem vindo</Text>
+    <Text style={styles.text}>
+        Para continuar, é necessário que você informe seu usuário no GitHub
+    </Text>
 
-        <View style={styles.form}>
-            <TextInput
-                style={styles.input}
-                autoCapitalize="none"
-                autoCorrect={false}
-                placeholder="Digite seu usuário"
-                underlineColorAndroid="transparent"
-            />
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="transparent"
+      />
 
-            <TouchableOpacity style={styles.button} onPress={() => {}}>
-                <Text style={styles.buttonText}>Continuar</Text>
-            </TouchableOpacity>
-        </View>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Continuar</Text>
+      </TouchableOpacity>
     </View>
+  </View>
 );
 
 Welcome.navigationOptions = {
