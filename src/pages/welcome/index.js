66632bde2c2a5ca348720d2c5e3e309be02638ad
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-06 21:49:26 -0300

    Styling Welcome's screen

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles';
 
-const Welcome = () => <View />;
+const Welcome = () => (
+    <View style={styles.container}>
+        <Text style={styles.title}>Bem vindo</Text>
+        <Text style={styles.text}>
+            Para continuar, é necessário que você informe seu usuário no GitHub
+        </Text>
+
+        <View style={styles.form}>
+            <TextInput
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Digite seu usuário"
+                underlineColorAndroid="transparent"
+            />
+
+            <TouchableOpacity style={styles.button} onPress={() => {}}>
+                <Text style={styles.buttonText}>Continuar</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+);
+
+Welcome.navigationOptions = {
+  header: null,
+};
 
 export default Welcome;
