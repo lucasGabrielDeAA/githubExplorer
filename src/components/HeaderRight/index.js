c0e2799f4d279d259b3ef3d2f70308d6ec6b0123
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-13 17:43:29 -0300

    Logout functionality with navigation redirect added

--- src/components/HeaderRight/index.js
+++ src/components/HeaderRight/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { TouchableOpacity, AsyncStorage } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import PropTypes from 'prop-types';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
-const logout = () => {
-
+class HeaderRight extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
+  logout = async () => {
+    await AsyncStorage.clear();
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Welcome' }),
+      ],
+    });
+    this.props.navigation.dispatch(resetAction);
+  }
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.logout()}>
+        <Icon name="exchange" size={16} style={styles.icon} />
+      </TouchableOpacity>
+    );
+  }
 }
-
-const HeaderRight = () => (
-  <TouchableOpacity onPress={() => logout()}>
-    <Icon name="exchange" size={16} style={styles.icon} />
-  </TouchableOpacity>
-);
 
 export default HeaderRight;
