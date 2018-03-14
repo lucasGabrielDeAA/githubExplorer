51fbda94cd0006e4f133457a8b8f10abdc384946
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-14 17:40:32 -0300

    Stylizing repositories's screen

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-
+import {
+  View,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList,
+  Text,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import RepositoryItem from './components/RepositoryItem';
+
+import api from 'services/api';
+
+import styles from './styles';
 
 class Repositories extends Component {
   static navigationOptions = {
@@ -9,8 +19,46 @@ class Repositories extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    const username = await AsyncStorage.getItem('@Github:username');
+    const response = await api.get(`/users/${username}/repos`);
+
+    this.setState({
+      data: response.data,
+      loading: false,
+    });
+  }
+
+  renderListItem = ({ item }) => (
+    <RepositoryItem repository={item} />
+  )
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  )
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        { this.state.loading ?
+          <ActivityIndicator style={styles.loading} />
+          : this.renderList()
+        }
+      </View>
+    )
   }
 }
 
---
+++ src/pages/repositories/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+import { metrics } from 'styles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  loading: {
+    marginTop: metrics.basePadding,
+  },
+});
+
+export default styles;
