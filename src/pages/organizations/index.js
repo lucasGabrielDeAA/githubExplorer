411c89e2ab6353603aea8340d1548b8679f0c3a8
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-14 17:56:09 -0300

    Organizations's screen adjustments

--- src/pages/organizations/index.js
+++ src/pages/organizations/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-
+import {
+  View,
+  AsyncStorage,
+  ActivityIndicator,
+  Text,
+  FlatList,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+import api from 'services/api';
+
+import styles from './styles';
 
 class Organizations extends Component {
   static navigationOptions = {
@@ -9,8 +18,49 @@ class Organizations extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    const username = await AsyncStorage.getItem('@Github:username');
+    const response = await api.get(`/users/${username}/orgs`);
+
+    console.tron.log(response);
+
+    this.setState({
+      data: response.data,
+      loading: false,
+    });
+  }
+
+  renderListItem = ({ item }) => {
+    console.tron.log(item);
+    return null;
+  }
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  )
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        { this.state.loading ?
+          <ActivityIndicator style={styles.loading} />
+          : this.renderList()
+        }
+      </View>
+    );
   }
 }
 
